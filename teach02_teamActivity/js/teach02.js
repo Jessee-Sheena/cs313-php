2187a4c97237b0910b01daf3127a7326d7803eb6
Sheena Jessee <calalilym@yahoo.com>
2019-01-15 12:52:48 -0700

    adding teach02

---
+++ teach02_teamActivity/js/teach02.js
@@ -0,0 +1,21 @@
+﻿// Your code here!
+function clicked() {
+	alert("CLICKED!");
+}
+/*function changeTheColor() {
+	var dom = document.getElementById("colorChange").value;
+	document.getElementById("firstDiv").style.color = dom;
+}*/
+$(document).ready(function () {
+	$("#colorButton").click(function () {
+		$("#firstDiv").attr("style", "color: " + $("#colorChange").val());
+	});
+});
+
+$(document).ready(function () {
+	$("#thirdButton").click(function () {
+		$("#thirdDiv").fadeToggle('slow');
+
+	});
+});
+
