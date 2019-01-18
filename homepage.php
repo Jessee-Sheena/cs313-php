5254660f211c9257261b974f2ccf804d4d8344b5
Sheena Jessee <calalilym@yahoo.com>
2019-01-18 12:20:24 -0700

    assignment 2

---
+++ homepage.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Sheena Jessee's Home Page</title>
+    <link rel="stylesheet" href="homepage.css" />
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
+    <script src="scripts/homepage.js"></script>
+</head>
+<body>
+    <header>
+        <div id="headerBox">
+            <h1>
+                Sheena Jessee's
+            </h1>
+            <h3>Home Page</h3>
+        </div>
+
+    </header>
+    <body>
+    <main>
+<?php
+
+ $scriptures = $_Post('scripture');
+ $kid = $_Post('kids');
+ $missions = $_Post('mission');
+ $deserts = $_Post('desert');
+ $colors = $_Post('color');
+ $dream = $_Post('dreams');
+ $sibling = $_Post('siblings');
+ $languages = $_Post('language');
+ $teeths = $_Post('teeth');
+
+ echo "What is my favorite scripture?";
+ if ($scriptures == 'Proverbs 3:5-6') {
+   echo"Correct My favorite Scripture is" . $scriptures;
+}else {
+   echo "Sorry your answer of" . $scriptures . "was not correct! The answer is Proverbs 3:5-6.";
+}
+ echo "How many kids do I have?";
+ if ($kid == '2 girls') {
+   echo"Correct the answer is " . $kid;
+}else {
+   echo "Sorry your answer of" . $kid . "was not correct! The answer is 2 girls.";
+}
+ echo "Where would I most like to go on a senior mission to?";
+ if ($missions == 'Africa') {
+   echo"Correct the answer is " . $missions;
+}else {
+   echo "Sorry your answer of" . $missions . "was not correct! The answer is Africa.";
+}
+ echo "What is my favorite desert?";
+ if ( $deserts == 'Chocolate') {
+   echo"Correct the answer is " . $deserts;
+}else {
+   echo "Sorry your answer of" . $deserts . "was not correct! The answer is Chocolate.";
+}
+ echo "What is my favorite color?";
+ if ( $colors == 'Orange') {
+   echo"Correct the answer is " . $colors;
+}else {
+   echo "Sorry your answer of" . $colors . "was not correct! The answer is Orange.";
+}
+ echo "When I was a kid I wanted to be a ___________ when I grew up?";
+ if ( $dream == 'Archeologist') {
+   echo"Correct the answer is " . $dream;
+}else {
+   echo "Sorry your answer of" . $dream . "was not correct! The answer is Archeologist.";
+}
+ echo "How many siblings do I have?";
+ if ( $sibling == '7') {
+   echo"Correct the answer is " . $sibling;
+}else {
+   echo "Sorry your answer of" . $sibling . "was not correct! The answer is 7.";
+}
+ echo "What language do I want to learn fluently?";
+ if (  $languages == 'French') {
+   echo"Correct the answer is " . $languages;
+}else {
+   echo "Sorry your answer of" . $languages . "was not correct! The answer is French.";
+}
+ echo "How old was I when I lost my last baby tooth?";
+ if (  $teeths == '13') {
+   echo"Correct the answer is " . $teeths;
+}else {
+   echo "Sorry your answer of" . $teeths . "was not correct! The answer is 13.";
+}
+
+?>
+  </main>
+    <footer> &copy; 2019 Sheena Jessee</footer>
+
+</body>
+</html>
