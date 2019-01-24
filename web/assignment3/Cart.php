44f9681456a029ccfcb647a86364cc1b4316933e
Sheena Jessee <calalilym@yahoo.com>
2019-01-24 08:31:24 -0700

    cart

---
+++ web/assignment3/Cart.php
@@ -0,0 +1,18 @@
+<?php 
+  echo "hello world";
+/*session_start();
+
+  /* $products = array();
+   $prices = array();
+   
+
+   $name = $_POST['name'];
+   $cost = $POST['cost'];
+   array_push($products, $name);
+   array_push($prices, $cost);
+   
+   if(!isset($_SESSION['total'])) {
+       $_SESSION['total'] = 0;
+       $_SESSION['producst'];
+   }*/
+ ?>
--- web/assignment3/viewCart.php
+++ web/assignment3/viewCart.php
@@ -1,18 +1,1 @@
-<?php 
-  echo "hello world";
-/*session_start();
-
-  /* $products = array();
-   $prices = array();
-   
-
-   $name = $_POST['name'];
-   $cost = $POST['cost'];
-   array_push($products, $name);
-   array_push($prices, $cost);
-   
-   if(!isset($_SESSION['total'])) {
-       $_SESSION['total'] = 0;
-       $_SESSION['producst'];
-   }*/
- ?>
+hello world
