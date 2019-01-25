3767daaf172a5b60f7aa8fe7422eb76a5ae7c7ff
Sheena Jessee <calalilym@yahoo.com>
2019-01-25 09:35:04 -0700

    Get and Post

--- web/assignment3/Cart.php
+++ web/assignment3/Cart.php
@@ -1,6 +1,24 @@
 <?php 
- echo $_SERVER['REQUEST_METHOD'];
+ 
+ $id = $_GET['name'];
+ $price = $_GET["cost"];
+ echo "name of item" . $id;
+ /*session_start();
 
- // $id = $_POST['name'];
- // echo "name of item" . $id;
+  $products = array();
+  $prices = array();
+   
+
+   $name = $_POST['name'];
+   $cost = $_POST['cost'];
+   array_push($products, $name);
+   array_push($prices, $cost);
+   
+   if(!isset($_SESSION['total'])) {
+       $_SESSION['total'] = 0;
+       $_SESSION['producst'];
+   }
+   echo "$products[0]";
+   echo "$prices[0]";*/ integrator
+
 ?>
