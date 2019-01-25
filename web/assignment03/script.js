71f69e1bfddc52c7c891704a2493f5e7ba302ce1
Sheena Jessee <calalilym@yahoo.com>
2019-01-25 12:26:12 -0700

    no idea what I am doing

--- web/assignment03/script.js
+++ web/assignment03/script.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 		var price = $(this).val();
 		$.ajax({
 			type: 'POST',
-			url: '/assignment03/cart.php/',
+			url: '/assignment03/cart',
 			
 			data: { name: id, cost: price },
 			success: function (response) {
