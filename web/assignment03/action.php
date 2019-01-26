2e14afa1402f13150ff83c71f21056faca081a16
Sheena Jessee <calalilym@yahoo.com>
2019-01-26 13:29:17 -0700

    more changes

--- web/assignment03/action.php
+++ web/assignment03/action.php
@@ -7,27 +7,6 @@ $action = $_POST['action'];
 $quantity = $_POST['quantity'];
 $id = $_POST['id'];
 echo $id;
-if(!empty($action)) {
-switch ($action) {
-    case "add":
-         if(!empty($quantity)) {
-           $theProduct = $itemArray[$id];
-           $productArray = array($theProduct[$id] => array('name'=>$theProduct['name'], 'quantitiy'=>$quantity, 'price'=>$theProduct['price'], 'image'=>$theProduct['image'] ));
-           if(!empty($_SESSION['cart'])) {
-                $cartArray = array_keys($_SESSION['cart']);
-                if(!in_array($theProduct[$id], $cartArray)) {
-                   foreach($_SESSION['cart'] as $key => $value) {
-                         if($theProduct == $key) {
-                            $theProduct[$id];
-                         }
-                   }
-                }
-              
-           }
-           else {
-              $_SESSION['cart'] = $productArray;
-           }
-         }
-    }
-}
+$_SESSION['id'] =$id;
+echo $_SESSION['id'];
  ?>
