375670dbfdb876cb6e13b2c71c5f0993cf30326b
Sheena Jessee <calalilym@yahoo.com>
2019-01-31 09:25:48 -0700

    added sql folder

---
+++ sql/recipedatabase.sql
@@ -0,0 +1,27 @@
+﻿CREATE TABLE "user"(
+   user_id serial PRIMARY KEY,
+   user_name VARCHAR(80),
+   email VARCHAR(80)
+);
+
+CREATE TABLE "recipe" (
+   recipe_id serial PRIMARY KEY,
+   recipe_name VARCHAR(80),
+   recipe_description VARCHAR(80),
+   instructions text,
+   cook_time INT,
+   prep_time INT,
+   user_id INT REFERENCES "user"(user_id)
+);
+CREATE TABLE "ingredients" (
+   ingredient_id serial PRIMARY KEY,
+   ingredient_name VARCHAR(80),
+   amount INT
+);
+CREATE TABLE "joined" (
+  joined_id serial PRIMARY KEY,
+  ingredient_id INT REFERENCES "ingredients" (ingredient_id),
+  recipe_id INT REFERENCES "recipe"(recipe_id)
+
+);
+
