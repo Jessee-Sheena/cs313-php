faf004b510aff2771b5966fc6ebf2027004d089b
Sheena Jessee <calalilym@yahoo.com>
2019-02-08 20:51:57 -0700

    fixed the footer

--- web/project1/recipeInfo.php
+++ web/project1/recipeInfo.php
@@ -6,30 +6,33 @@
  
   foreach ($db->query("SELECT * FROM recipe WHERE recipe_id = " .$id ) as $row)
         {?>
+        <div id="infoDiv">
            <h2><?php echo $row['recipe_name']?></h2>
            <img src="<?php echo $row['image'] ?>" alt="food"/>
-          <p><?php echo $row['recipe_description'] ?></p>
-    <table>
-        <thead>
-            <tr>
-                <td>Cuisine</td>                
-                <td>Prep Time</td>
-                <td>Cook Time</td>
-                <td>Total Time</td>
-                <td>Serving Size</td>
-                <td>Calories per Serving</td>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td><?php echo $row['cuisine'] ?></td>                
-                <td><?php echo $row['prep_time'] ?></td>
-                <td><?php echo $row['cook_time'] ?></td>
-                <td><?php echo $row['total_time'] ?></td>
-                <td><?php echo $row['serving_size'] ?></td>
-                <td><?php echo $row['calories'] ?></td>
-            </tr> 
-
+           <p><?php echo $row['recipe_description'] ?></p>
+           <table>
+              <thead>
+                <tr>
+                   <td>Cuisine</td>                
+                   <td>Prep Time</td>
+                   <td>Cook Time</td>
+                   <td>Total Time</td>
+                   <td>Serving Size</td>
+                   <td>Calories per Serving</td>
+               </tr>
+             </thead>
+           <tbody>
+               <tr>
+                   <td><?php echo $row['cuisine'] ?></td>                
+                   <td><?php echo $row['prep_time'] ?></td>
+                   <td><?php echo $row['cook_time'] ?></td>
+                   <td><?php echo $row['total_time'] ?></td>
+                   <td><?php echo $row['serving_size'] ?></td>
+                   <td><?php echo $row['calories'] ?></td>
+               </tr> 
+            </tbody>
+         </table>
+    </div>
         
      <?php  }  ?>
 
