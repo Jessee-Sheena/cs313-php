2da11f92dcccae51ca6a821ad5955a9039d76a4e
Sheena Jessee <calalilym@yahoo.com>
2019-02-08 20:42:19 -0700

    it will work

--- web/project1/recipeInfo.php
+++ web/project1/recipeInfo.php
@@ -4,31 +4,10 @@
    $db=getDb();
    $id = $_GET['id'];   
  
-  foreach ($db->query("SELECT * FROM recipe WHERE id = 2 ") as $row)
+  foreach ($db->query("SELECT * FROM recipe WHERE recipe_id = " .$id ) as $row)
         {
           echo "<h2>". $row['recipe_name'] ."?></h2>";
-         /* <img src="<?php echo $row['image'] ?>" alt="food"/>
-          <p><?php echo $row['recipe_description'] ?></p>
-    <table>
-        <thead>
-            <tr>
-                <td>Cuisine</td>                
-                <td>Prep Time</td>
-                <td>Cook Time</td>
-                <td>Total Time</td>
-                <td>Serving Size</td>
-                <td>Calories per Serving</td>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td><?php echo $row['cuisine'] ?></td>                
-                <td><?php echo $row['prep_time'] ?></td>
-                <td><?php echo $row['cook_time'] ?></td>
-                <td><?php echo $row['total_time'] ?></td>
-                <td><?php echo $row['serving_size'] ?></td>
-                <td><?php echo $row['calories'] ?></td>
-            </tr> */
+        
        }  ?>
 
   <?php
