6b2a352e76c04640d6faf1d3a0c2e4c7eb7a5e38
Sheena Jessee <calalilym@yahoo.com>
2019-02-08 20:25:43 -0700

    will it work

--- web/project1/recipeInfo.php
+++ web/project1/recipeInfo.php
@@ -2,10 +2,9 @@
    include_once "header.php";
    require "config.php";
    $db=getDb();
-   $id = $_GET['id'];
-   echo $id;
- ?>
- <!-- foreach ($db->query("SELECT * FROM recipe WHERE id = " . $id) as $row)
+   $id = $_GET['id'];   
+ 
+  foreach ($db->query("SELECT * FROM recipe WHERE id = " . $id) as $row)
         {?>
           <h2><?php echo $row['recipe_name']?></h2>
     <img src="<?php echo $row['image'] ?>" alt="food"/>
@@ -29,10 +28,8 @@
                 <td><?php echo $row['total_time'] ?></td>
                 <td><?php echo $row['serving_size'] ?></td>
                 <td><?php echo $row['calories'] ?></td>
-            </tr> -->
-       <?php //}  ?>*/
-
-
+            </tr> 
+       <?php }  ?>
 
   <?php
    include_once "footer.php";
