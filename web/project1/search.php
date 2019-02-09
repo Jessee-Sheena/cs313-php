3c12b2eca0688196d2c641820fbde8a0c209b3af
Sheena Jessee <calalilym@yahoo.com>
2019-02-09 10:25:15 -0700

    added ingredients search

--- web/project1/search.php
+++ web/project1/search.php
@@ -71,7 +71,23 @@
                }							
 				break;
 			case 'ingredients':
-				
+				$ingredient1 = $_POST['ingredient1'];
+                $ingredient2 = $_POST['ingredient2'];
+                $ingredient3 = $_POST['ingredient3'];
+                $ingredient4 = $_POST['ingredient4'];
+               foreach ($db->query("SELECT
+                          r.recipe_id,
+                          r.recipe_name,
+                          FROM ingredients AS i
+                          JOIN recipe_ingredients AS q ON q.ingredient_id = i.ingredient_id
+                          JOIN recipe AS r ON r.recipe_id = q.recipe_id
+                          WHERElower( i.ingredient_name) = lower ( '" . $ingredient1 ."')") as $row)
+               {
+                              
+                echo '<a href="recipeInfo.php?id='.$row['recipe_id'].'">'. $row['recipe_name'] . '</a>';
+                
+               }			
+
 				break;
                 }
        
