4daa92a639f353d6746121466a938308334e63e8
Sheena Jessee <calalilym@yahoo.com>
2019-02-08 20:06:45 -0700

    will it work

--- web/project1/test.php
+++ web/project1/test.php
@@ -2,8 +2,8 @@
    require "config.php";
    $db = getDb();
 
-    foreach ($db->query("SELECT * FROM recipe" ) as $row)
-        {
-          echo '<p><b>' . $row['recipe_name'] . '</p>';
-        }
+    //foreach ($db->query("SELECT * FROM recipe" ) as $row)
+      //  {
+       //   echo '<p><b>' . $row['recipe_name'] . '</p>';
+       // }
  ?>
