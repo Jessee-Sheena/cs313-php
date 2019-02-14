45305a1a0abc861ede452b633fe7f1b8f4dba09d
Sheena Jessee <calalilym@yahoo.com>
2019-02-14 13:07:57 -0700

    trying to get th upload to work

--- web/project1/submit.php
+++ web/project1/submit.php
@@ -2,28 +2,13 @@
 session_start();
 include_once "header.php";
 require "config.php";
-$db = getDb();
+$db=getDb();
 
-   $name = htmlspecialchars($_POST['recipeName']);
-   $description = htmlspecialchars($_POST['recipeDescription']);
-   $cookTime = htmlspecialchars($_POST['cook_Time']);
-   $prepTime = htmlspecialchars($_POST['prep_Time']);
-   $totalTime = htmlspecialchars($_POST['total_Time']);
-   $serving = htmlspecialchars($_POST['serving_size']);
-   $calories = htmlspecialchars($_POST['calories']);
-   $cuisine = htmlspecialchars($_POST['cuisine']);
-   $recipeImage_path = htmlspecialchars('images/'.$_FILES['recipeImage']['name']);
-   
-  if(preg_match("!image!", $_FILES['recipeImage']['type'])) {
-    if (copy($_FILES['recipeImage']['tmp_name'], $recipeImage_path)) {
-         $db->query("INSERT INTO recipe (recipe_name, recipe_description, cook_time, prep_time, cuisine, total_time, serving_size, calories, image) VALUES ('".$name."', '".$description. "', '".$cookTime."', '".$prepTime."', '".$cuisine."', '".$totalTime."', '".$serving."', '".$calories."', '".$recipeImage_path."');");
-      }
-   }
 
 ?>
   <div id="submitForm">
         <h1>Recipe Submission</h1>
-        <form action="submit.php" method="post" id="recipeForm">
+        <form action="submit.php" method="post" id="recipeForm" enctype="multipart/form-data">
             <label for="recipeName">Recipe Name: </label>
             <input type="text" id="recipeName" name="recipeName" required />
             <label for="recipeDescription">Description</label>
@@ -55,6 +40,24 @@ $db = getDb();
             </div>
         </form>
     </div>
+    <?php
+    
+   $name = htmlspecialchars($_POST['recipeName']);
+   $description = htmlspecialchars($_POST['recipeDescription']);
+   $cookTime = htmlspecialchars($_POST['cook_Time']);
+   $prepTime = htmlspecialchars($_POST['prep_Time']);
+   $totalTime = htmlspecialchars($_POST['total_Time']);
+   $serving = htmlspecialchars($_POST['serving_size']);
+   $calories = htmlspecialchars($_POST['calories']);
+   $cuisine = htmlspecialchars($_POST['cuisine']);
+   $recipeImage_path = htmlspecialchars('images/'.$_FILES['recipeImage']['name']);
+   print_r($_FILES);
+  if(preg_match("!image!", $_FILES['recipeImage']['type'])) {
+    if (copy($_FILES['recipeImage']['tmp_name'], $recipeImage_path)) {
+         $db->query("INSERT INTO recipe (recipe_name, recipe_description, cook_time, prep_time, cuisine, total_time, serving_size, calories, image) VALUES ('".$name."', '".$description. "', '".$cookTime."', '".$prepTime."', '".$cuisine."', '".$totalTime."', '".$serving."', '".$calories."', '".$recipeImage_path."');");
+      }
+   }
+ ?>
 
 
 <?php
