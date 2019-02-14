2954bf6bf69871080436270428d1dbd18a6b995f
Sheena Jessee <calalilym@yahoo.com>
2019-02-14 10:20:51 -0700

    testing

--- web/project1/submit.php
+++ web/project1/submit.php
@@ -3,11 +3,11 @@ session_start();
 include_once "header.php";
 require "config.php";
 $db = getDb();
-/*if($_SERVER['REQUEST_METHOD']=='POST') {
+if($_SERVER['REQUEST_METHOD']=='POST') {
    $name = htmlspecialchars($_POST['recipeName']);
    $description = htmlspecialchars($_POST['recipeDescription']);
    $cookTime = htmlspecialchars($_POST['cook_Time']);
-   $prepTime = htmlspecialchars($_POST['prep_Time']);
+  /* $prepTime = htmlspecialchars($_POST['prep_Time']);
    $totalTime = htmlspecialchars($_POST['total_Time']);
    $serving = htmlspecialchars($_POST['serving_size']);
    $calories = htmlspecialchars($_POST['calories']);
@@ -18,8 +18,8 @@ $db = getDb();
       if (copy($_FILES['recipeImage']['tmp_name'], $recipeImage_path)) {
          $db->query("INSERT INTO recipe (recipe_name, recipe_description, cook_time, prep_time, cuisine, total_time, serving_size, calories, image) VALUES ($name, $description, $cookTime, $prepTime, $cuisine, $totalTime, $serving, $calories, $recipeImage_path);");
       
-    }
-}*/
+    }*/
+}
 ?>
   <div id="submitForm">
         <h1>Recipe Submission</h1>
