ad758a55c6ccc5c529f57548815997fcaeedf821
Sheena Jessee <calalilym@yahoo.com>
2019-02-16 13:01:59 -0700

    will i still get 0

--- web/project1/ingredient.php
+++ web/project1/ingredient.php
@@ -3,27 +3,26 @@ session_start();
 
 require "config.php";
 $db=getDb();
-$ingredient = "word";
-$unit = "people";
-$measurement;
-//foreach ($db->query(" SELECT recipe_id FROM recipe ORDER BY recipe_id DESC LIMIT 1;") as $row)
-//{
+
+foreach ($db->query(" SELECT recipe_id FROM recipe ORDER BY recipe_id DESC LIMIT 1;") as $row)
+{
               // $_SESSION['tempId'] = $row['recipe_id'];        
                 
-// }	
+ }	
 
  if(isset($_POST['ingredient'])) {
       $ingredient = htmlspecialchars($_POST['ingredient']);
-
+      echo $ingredient;
       $db->query("INSERT INTO ingredients (ingredient_name)
       SELECT '".$ingredient ."'
       WHERE NOT EXISTS (SELECT * FROM ingredients WHERE ingredient_name = '". $ingredient. "');");
    }
    if(isset($_POST['measurement'])) {
       $unit = htmlspecialchars($_POST['measurement']);
+      echo $unit;
       $db->query("INSERT INTO measurement (unit)
       SELECT '".$unit ."'
       WHERE NOT EXISTS (SELECT * FROM measurement WHERE unit = '". $unit. "');");
    }
- // echo $ingredient + " " + $unit;
+ 
 ?>
