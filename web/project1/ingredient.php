115bdd570ee6f32cfb5eb3b4970de6b1e68fc15d
Sheena Jessee <calalilym@yahoo.com>
2019-02-16 13:54:57 -0700

    fixed the spaces

--- web/project1/ingredient.php
+++ web/project1/ingredient.php
@@ -12,7 +12,7 @@ foreach ($db->query(" SELECT recipe_id FROM recipe ORDER BY recipe_id DESC LIMIT
 
  if(isset($_POST['ingredient'])) {
       $ingredient = htmlspecialchars($_POST['ingredient']);
-      echo $ingredient + " ";
+      echo  $ingredient . " ";
       $db->query("INSERT INTO ingredients (ingredient_name)
       SELECT '".$ingredient ."'
       WHERE NOT EXISTS (SELECT * FROM ingredients WHERE ingredient_name = '". $ingredient. "');");
@@ -25,7 +25,7 @@ foreach ($db->query(" SELECT recipe_id FROM recipe ORDER BY recipe_id DESC LIMIT
    if(isset($_POST['measurement'])) {
       $unit = htmlspecialchars($_POST['measurement']);
       $quantity = htmlspecialchars($_POST['amount']);
-      echo  $quantity + " " + $unit;
+      echo  $quantity . " " . $unit;
       $db->query("INSERT INTO measurement (unit)
       SELECT '".$unit ."'
       WHERE NOT EXISTS (SELECT * FROM measurement WHERE unit = '". $unit. "');");
