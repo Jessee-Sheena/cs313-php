4e3004078d291bc5e946b2a880ee074b6fd99c58
Sheena Jessee <calalilym@yahoo.com>
2019-02-16 11:21:31 -0700

    trying to get the last id

--- web/project1/ingredient.php
+++ web/project1/ingredient.php
@@ -1,9 +1,11 @@
 <?php 
 session_start();
-echo $_SESSION['tempId'];
+
 require "config.php";
 $db=getDb();
-//$db->prepare("SELECT id,value,card FROM my_table ORDER BY id DESC LIMIT 1");
+foreach ($db->prepare("SELECT recipe_id FROM recipe ORDER BY id DESC LIMIT 1") as $row); {
+        $_SESSION['tempId'] = $row['recipe_id'];
+}
 
  if(isset($_POST['ingredient'])) {
       $ingredient = htmlspecialchars($_POST['ingredient']);
@@ -11,4 +13,5 @@ $db=getDb();
       SELECT '".$ingredient ."'
       WHERE NOT EXISTS (SELECT * FROM ingredients WHERE ingredient_name = '". $ingredient. "');");
    }
+   echo $_SESSION['tempId'];
 ?>
