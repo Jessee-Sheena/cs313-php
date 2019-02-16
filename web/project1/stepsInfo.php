a29cf15632bd938e17589665cfa6b7e4c938d8d3
Sheena Jessee <calalilym@yahoo.com>
2019-02-16 15:28:18 -0700

    save it

--- web/project1/stepsInfo.php
+++ web/project1/stepsInfo.php
@@ -3,16 +3,16 @@ session_start();
 require "config.php";
 $db=getDb();
 
-echo $_SESSION['recipeId'];
+
 
 if(isset($_POST['steps'])) {
       $step = htmlspecialchars($_POST['steps']);  
       $stepNum = htmlspecialchars($_POST['stepNum']);   
       echo $step;
-     /* $db->query("INSERT INTO steps (recipe_id, step_number, step_instruction)
+     $db->query("INSERT INTO steps (recipe_id, step_number, step_instruction)
       VALUES ( '". $_SESSION['recipeId'] ."', ' " . $stepNum ."', ' " . $step . "');");
       echo $stepNum . " " . $step;
-      }*/
+      }
       
 
       }
