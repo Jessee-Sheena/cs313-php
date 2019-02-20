df7efdae4bc25e7ffc97e9c233c1dd468fecb36d
Sheena Jessee <calalilym@yahoo.com>
2019-02-20 12:27:32 -0700

    non

--- web/team07/sign_up.php
+++ web/team07/sign_up.php
@@ -2,23 +2,35 @@
 session_start();
 require "config.php";
 $db=getDb();
+if(isset($_POST['password']) AND isset($_POST['password2']) AND isset($_POST['username'])) {
+  if($_POST['password']== $_POST['password2']) {
+  $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
+  $query =$db->prepare("INSERT INTO \"userTeam\" (userName, user_password) VALUES ('".$_POST['username']."','".$hashedPassword."');");
+  $query->execute();
+  header('Location: sign_in.php ');
+  die();
+} else{
+  echo "<p style=\"color: red\"> Passwords do not match.</p>";
+  ?> <style> .asterisk {display: inline;} </style> <?php
+}
+
+}
 
       
 ?>
-<form action="sign_up.php" method="post">
-<label for="username"> Create a username: </label>
+<style>
+.asterisk {
+   display: none;
+}
+
+</style>
+<form action="sign_up.php" method="post"> 
+<span class="asterisk user" > * </span><label for="username"> Create a username: </label>
 <input type="text" name="username" id="username" required>
-<label for="password"> Create a password: </label>
+<span class="asterisk password" > * </span><label for="password"> Create a password: </label>
 <input type="password" name="password" id="password" required>
+<span class="asterisk password" > * </span><label for="password2"> Verify Password </label>
+<input type="password" name="password2" id="password2" required>
 <input type="submit">
 </form>
-<?php
-if(isset($_POST['username'])) {
-$hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
-$query =$db->prepare("INSERT INTO \"userTeam\" (userName, user_password) VALUES ('".$_POST['username']."','".$hashedPassword."');");
-$query->execute();
-header('Location: sign_in.php ');
-die();
 
-}
- ?>
--- web/team07/welcome.php
+++ web/team07/welcome.php
@@ -4,7 +4,7 @@ require "config.php";
 $db=getDb();
 
     if(!empty($_SESSION['user'])) {
-    echo "<h1> Welcome". $_SESSION['user']. "!";
+    echo "<h1> Welcome ". $_SESSION['user']. "!";
 }
 else {
 header('Location: sign_in.php');
