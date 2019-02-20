9f122d01f018e88ff3bce4c2fd21068fe8e70784
Sheena Jessee <calalilym@yahoo.com>
2019-02-20 12:42:14 -0700

    ;

--- web/team07/sign_up.php
+++ web/team07/sign_up.php
@@ -11,11 +11,14 @@ require "config.php";
 $db=getDb();
 if(isset($_POST['password']) AND isset($_POST['password2']) AND isset($_POST['username'])) {
   if($_POST['password']== $_POST['password2']) {
+  $length = strlen($_POST['password']);
+  if($length > 7 AND 1 === preg_match('~[0-9]~', $_POST['password'])) {
   $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
   $query =$db->prepare("INSERT INTO \"userTeam\" (userName, user_password) VALUES ('".$_POST['username']."','".$hashedPassword."');");
   $query->execute();
   header('Location: sign_in.php ');
   die();
+  }
 } else{
   echo "<p style=\"color: red\"> Passwords do not match.</p>";
   ?> <style>  .password {display: inline;} </style> <?php
@@ -29,7 +32,7 @@ if(isset($_POST['password']) AND isset($_POST['password2']) AND isset($_POST['us
 <form action="sign_up.php" method="post"> 
 <span class="asterisk user" > * </span><label for="username"> Create a username: </label>
 <input type="text" name="username" id="username" required>
-<span class="asterisk password" > * </span><label for="password"> Create a password: </label>
+<span class="asterisk password" > * </span><label for="password"> Create a password: (at least 7 charachters)</label>
 <input type="password" name="password" id="password" required>
 <span class="asterisk password" > * </span><label for="password2"> Verify Password </label>
 <input type="password" name="password2" id="password2" required>
