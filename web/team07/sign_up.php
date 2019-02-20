7e49289824af662f647a9c7d5faefee265c9402a
Sheena Jessee <calalilym@yahoo.com>
2019-02-20 11:13:43 -0700

    l

--- web/team07/sign_up.php
+++ web/team07/sign_up.php
@@ -5,8 +5,16 @@ $db=getDb();
 
       
 ?>
-
+<form action="sign_up.php" method="post">
 <label for="username"> Create a username: </label>
 <input type="text" name="username" id="username" required>
 <label for="password"> Create a password: </label>
 <input type="password" name="password" id="password" required>
+<input type="submit">
+</form>
+<?php
+if(isset($_POST['username'])) {
+$db-> prepare("INSERT INTO userTeam (userName, user_password') VALUES ('".$_POST['username']."','".$_POST['user_password']."');");
+
+}
+ ?>
--- web/team07/teamDb.sql
+++ web/team07/teamDb.sql
@@ -1,5 +1,5 @@
-CREATE TABLE "user" (
+CREATE TABLE "userTeam" (
    id serial PRIMARY KEY,
    userName VARCHAR(80),
-   user_password VARCHAR(80),
+   user_password VARCHAR(80)
 ); 
