a341939718218f02f5f173861da5e4816d19632f
Sheena Jessee <calalilym@yahoo.com>
2019-02-21 16:22:50 -0700

    added user

---
+++ web/project1/sign_in.php
@@ -0,0 +1,37 @@
+<?php 
+ session_start();
+   include_once "header.php";   
+   require "config.php";
+   $db=getDb();
+      
+?>
+<div>
+<form action="sign_in.php" method="post">
+<label for="username"> Enter username: </label>
+<input type="text" name="username" id="username" required>
+<label for="password"> Enter password: </label>
+<input type="password" name="password" id="password" required>
+<input type="submit">
+<a href="sign_up.php"> sign up </a>
+</form>
+</div>
+<?php
+$query = $db->query("SELECT user_name, user_password FROM \"user\" WHERE user_name = '". $_POST['username']."';" );
+$query->execute(); 
+$user = $query->fetch();
+if (password_verify($_POST['password'],$user['password'])) {
+    $_SESSION['user'] = $_POST['username'];
+    header('Location: home.php');
+    die();
+   
+    
+}
+else {
+echo "Password Incorrect";
+}
+
+
+ ?>
+ <?php
+  } include_once "footer.php";
+ ?>
---
+++ web/project1/sign_up.php
@@ -0,0 +1,41 @@
+<?php 
+ session_start();
+   include_once "header.php";   
+   require "config.php";
+   $db=getDb();
+
+if(isset($_POST['password']) AND isset($_POST['password2']) AND isset($_POST['username'])) {
+  if($_POST['password']== $_POST['password2']) {
+     $length = strlen($_POST['password']);
+     if($length >= 7 AND 1 === preg_match('~[0-9]~', $_POST['password'])) {
+       $hashedPassword = password_hash($_POST['password'], PASSWORD_DEFAULT);
+       $query =$db->prepare("INSERT INTO \"user\" (user_name, password) VALUES ('".$_POST['username']."','".$hashedPassword."');");
+       $query->execute();
+       header('Location: sign_in.php ');
+       die();
+  }else {
+  echo "<p style=\"color: red\">Password is not long enough or does not contain a number.</p>";
+}
+} else{
+  echo "<p style=\"color: red\"> Passwords do not match.</p>";
+  ?> <style>  .password {display: inline;} </style> <?php
+}
+
+}
+
+      
+?>
+
+<form action="sign_up.php" method="post"> 
+<span class="asterisk user" > * </span><label for="username"> Create a username: </label>
+<input type="text" name="username" id="username" required>
+<span class="asterisk password" > * </span><label for="password"> Create a password: (at least 7 charachters)</label>
+<input type="password" name="password" id="password" pattern="(?=.*\d)[A-Za-z\d]{7,}" required>
+<span class="asterisk password" > * </span><label for="password2"> Verify Password </label>
+<input type="password" name="password2" id="password2" required>
+<input type="submit">
+</form
+
+<?php
+  } include_once "footer.php";
+ ?>
