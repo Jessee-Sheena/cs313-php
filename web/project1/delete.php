4791e1aa4b9e79df32370327c1a36ea3266c40e8
Sheena Jessee <calalilym@yahoo.com>
2019-02-21 13:14:15 -0700

    new

--- web/project1/delete.php
+++ web/project1/delete.php
@@ -2,7 +2,9 @@
    session_start();
    require "config.php";
    $db=getDb();
-  
+
+
+   $db->query("DELETE FROM recipe_ingredients WHERE recipe_id = '". $_SESSION['id']. "';");
    $db->query("DELETE FROM recipe WHERE recipe_id = '". $_SESSION['id']. "';");
    header('Location: recipelist.php');
  ?>
