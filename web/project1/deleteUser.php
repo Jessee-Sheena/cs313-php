7b1f3e0201578de28062a467863b99ddc8f108ed
Sheena Jessee <calalilym@yahoo.com>
2019-02-22 12:40:48 -0700

    destroy session

--- web/project1/deleteUser.php
+++ web/project1/deleteUser.php
@@ -1,17 +1,14 @@
 <?php 
 session_start();
-   include_once "header.php";   
+  
    require "config.php";
    $db=getDb();      
   
 $db->query("DELETE FROM recipe WHERE user_id = '" . $_SESSION['user_id']. "';");
 $db->query("DELETE FROM \"user\" WHERE user_id = '" . $_SESSION['user_id']. "';");
-
+session_destroy();
 header('Location: home.php');
 die();
 ?>
 
 
-<?php
-include_once "footer.php";
- ?>
