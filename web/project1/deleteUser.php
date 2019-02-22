82e0d32674eff2dc193bbe533d4837cb305bddd5
Sheena Jessee <calalilym@yahoo.com>
2019-02-22 12:49:17 -0700

    remove curly brace

--- web/project1/deleteUser.php
+++ web/project1/deleteUser.php
@@ -3,7 +3,7 @@
    include_once "header.php";   
    require "config.php";
    $db=getDb();      
-  if(!empty($_SESSION['user_id'])){}
+  if(!empty($_SESSION['user_id'])){
 $db->query("DELETE FROM recipe WHERE user_id = '" . $_SESSION['user_id']. "';");
 $db->query("DELETE FROM \"user\" WHERE user_id = '" . $_SESSION['user_id']. "';");
 session_destroy();
