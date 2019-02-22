2212fa2c96d7c7f054757d41e26aaaa0f8aa17a9
Sheena Jessee <calalilym@yahoo.com>
2019-02-22 11:16:41 -0700

    see the changes

--- web/project1/editUser.php
+++ web/project1/editUser.php
@@ -3,20 +3,7 @@
    include_once "header.php";   
    require "config.php";
    $db=getDb();
- ?>
-<div id="editUser">
-    <form action="editUser.php" method="post"> 
-       <span class="asterisk user" > * </span><label for="username"> Change your username: </label>
-       <input type="text" name="editUsername" id="editUsername" required>
-       <span class="asterisk password" > * </span><label for="editPassword"> Change your password: (at least 7 charachters)</label>
-       <input type="password" name="editPassword" id="editPassword" pattern="(?=.*\d)[A-Za-z\d]{7,}" required>
-       <span class="asterisk password" > * </span><label for="editPassword2"> Verify Password </label>
-       <input type="password" name="editPassword2" id="editPassword2" required>
-       <input type="submit">
-    </form
-</div>
-<?php
- if(isset($_POST['password']) AND isset($_POST['password2']) AND isset($_POST['username'])) {
+   if(isset($_POST['password']) AND isset($_POST['password2']) AND isset($_POST['username'])) {
   if($_POST['password']== $_POST['password2']) {
      $length = strlen($_POST['password']);
      if($length >= 7 AND 1 === preg_match('~[0-9]~', $_POST['password'])) {
@@ -37,4 +24,18 @@
 }
 
 }
+ ?>
+<div id="editUser">
+    <form action="editUser.php" method="post"> 
+       <span class="asterisk user" > * </span><label for="username"> Change your username: </label>
+       <input type="text" name="editUsername" id="editUsername" required>
+       <span class="asterisk password" > * </span><label for="editPassword"> Change your password: (at least 7 charachters)</label>
+       <input type="password" name="editPassword" id="editPassword" pattern="(?=.*\d)[A-Za-z\d]{7,}" required>
+       <span class="asterisk password" > * </span><label for="editPassword2"> Verify Password </label>
+       <input type="password" name="editPassword2" id="editPassword2" required>
+       <input type="submit">
+    </form
+</div>
+<?php
+ 
 ?>
