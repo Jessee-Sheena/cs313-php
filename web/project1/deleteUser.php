0e246356b5d8abe945b5c66e2bf8dc0cc29cc090
Sheena Jessee <calalilym@yahoo.com>
2019-02-22 12:23:55 -0700

    jdlkfj

--- web/project1/deleteUser.php
+++ web/project1/deleteUser.php
@@ -1,8 +1,9 @@
 <?php 
 session_start();
-require "config.php";
-$db=getDb();
-
-$db-query("DELETE FROM recipe WHERE user_id = '" . $_SESSION['user_id']. "';");
-$db-query("DELETE FROM \"user\" WHERE user_id = '" . $_SESSION['user_id']. "';");
+   include_once "header.php";   
+   require "config.php";
+   $db=getDb();      
+   echo $_SESSION['user_id'];
+//$db-query("DELETE FROM recipe WHERE user_id = '" . $_SESSION['user_id']. "';");
+//$db-query("DELETE FROM \"user\" WHERE user_id = '" . $_SESSION['user_id']. "';");
 ?>
--- web/project1/editUser.php
+++ web/project1/editUser.php
@@ -9,7 +9,7 @@
         $length = strlen($_POST['editPassword']);
         if($length >= 7 AND 1 === preg_match('~[0-9]~', $_POST['editPassword'])) {
            $hashedPassword = password_hash($_POST['editPassword'], PASSWORD_DEFAULT);
-           $db->query("UPDATE \"user\" SET user_name = '". $_POST['editUsername']. "' WHERE user_id = '" . $_SESSION['user_id']. "';");
+           $db->query("UPDATE \"user\" SET user_name = '". $_POST['editUsername']. "', password ='". $hashedPassword. "' WHERE user_id = '" . $_SESSION['user_id']. "';");
           header('Location: home.php ');
           die();
         }else {
