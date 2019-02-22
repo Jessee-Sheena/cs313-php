8b8627cd8d1fb0d0bd91445b2e1a5bf04ef57ef3
Sheena Jessee <calalilym@yahoo.com>
2019-02-22 09:28:09 -0700

    changes

--- web/project1/addIngredient.php
+++ web/project1/addIngredient.php
@@ -3,8 +3,30 @@ session_start();
 include_once "header.php";
 require "config.php";
 $db=getDb();
+   
 
-      
+   $name = htmlspecialchars($_POST['recipeName']);
+   $description = htmlspecialchars($_POST['recipeDescription']);
+   $cookTime = htmlspecialchars($_POST['cook_Time']);
+   $prepTime = htmlspecialchars($_POST['prep_Time']);
+   $totalTime = htmlspecialchars($_POST['total_Time']);
+   $serving = htmlspecialchars($_POST['serving_size']);
+   $calories = htmlspecialchars($_POST['calories']);
+   $cuisine = htmlspecialchars($_POST['cuisine']);
+   $recipeImage_path = htmlspecialchars('images/'.$_FILES['recipeImage']['name']);
+  
+  if(preg_match("!image!", $_FILES['recipeImage']['type'])) {
+       
+    if (copy($_FILES['recipeImage']['tmp_name'], $recipeImage_path)) {
+        
+         $userID = $db->query("SELECT user_id FROM \"user\" WHERE user_name ='" . $_SESSION['user'] . "';" );
+         echo $userID;
+         $db->query("INSERT INTO recipe (recipe_name, recipe_description, cook_time, prep_time, cuisine, total_time, serving_size, calories, image, user_id) VALUES ('".$name."', '".$description. "', '".$cookTime."', '".$prepTime."', '".$cuisine."', '".$totalTime."', '".$serving."', '".$calories."', '".$recipeImage_path."', '" . $userID . "');");
+              
+       
+   }
+   }
+ 
 ?>
     <div id="ingredientFormDiv">
         <form  method="post" id="ingredientForm">
@@ -45,30 +67,7 @@ $db=getDb();
          </ul>
         </div>
     </div>
-    <?php 
-   $name = htmlspecialchars($_POST['recipeName']);
-   $description = htmlspecialchars($_POST['recipeDescription']);
-   $cookTime = htmlspecialchars($_POST['cook_Time']);
-   $prepTime = htmlspecialchars($_POST['prep_Time']);
-   $totalTime = htmlspecialchars($_POST['total_Time']);
-   $serving = htmlspecialchars($_POST['serving_size']);
-   $calories = htmlspecialchars($_POST['calories']);
-   $cuisine = htmlspecialchars($_POST['cuisine']);
-   $recipeImage_path = htmlspecialchars('images/'.$_FILES['recipeImage']['name']);
-  
-  if(preg_match("!image!", $_FILES['recipeImage']['type'])) {
-       
-    if (copy($_FILES['recipeImage']['tmp_name'], $recipeImage_path)) {
-        
-         $userID = $db->query("SELECT user_id FROM \"user\" WHERE user_name ='" . $_SESSION['user'] . "';" );
-         echo $userID;
-         $db->query("INSERT INTO recipe (recipe_name, recipe_description, cook_time, prep_time, cuisine, total_time, serving_size, calories, image, user_id) VALUES ('".$name."', '".$description. "', '".$cookTime."', '".$prepTime."', '".$cuisine."', '".$totalTime."', '".$serving."', '".$calories."', '".$recipeImage_path."', '" . $userID . "');");
-              
-       
-   }
-   }
- 
-?>
+   
     
 <?php
  
