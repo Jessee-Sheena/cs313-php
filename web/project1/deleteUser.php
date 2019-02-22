c510ffb7ce03a2293362f642a03bb29fe85d2f87
Sheena Jessee <calalilym@yahoo.com>
2019-02-22 12:46:12 -0700

    delete user

--- web/project1/deleteUser.php
+++ web/project1/deleteUser.php
@@ -1,14 +1,19 @@
 <?php 
-session_start();
-  
+   session_start();
+   include_once "header.php";   
    require "config.php";
    $db=getDb();      
-  
+  if(!empty($_SESSION['user_id'])){}
 $db->query("DELETE FROM recipe WHERE user_id = '" . $_SESSION['user_id']. "';");
 $db->query("DELETE FROM \"user\" WHERE user_id = '" . $_SESSION['user_id']. "';");
 session_destroy();
 header('Location: home.php');
 die();
+} else
+echo "There is no kown user to delete"
 ?>
 
+<?php
+   include_once "footer.php";
+ ?>
 
