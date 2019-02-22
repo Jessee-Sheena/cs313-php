c603bd27271cc90af38bbc49edf5e1f6ba5d362c
Sheena Jessee <calalilym@yahoo.com>
2019-02-22 11:05:27 -0700

    silly bracket

--- web/project1/editUser.php
+++ web/project1/editUser.php
@@ -21,7 +21,7 @@
      $length = strlen($_POST['password']);
      if($length >= 7 AND 1 === preg_match('~[0-9]~', $_POST['password'])) {
        $hashedPassword = password_hash($_POST['editPassword'], PASSWORD_DEFAULT);
-       $db-query("UPDATE \"user\" SET user_name = '". $_POST['editUsername']. "', password = '". $hashedPassword ."' WHERE user_id = '" . $_SESSION['user_id']. "';");
+       $db->query("UPDATE \"user\" SET user_name = '". $_POST['editUsername']. "', password = '". $hashedPassword ."' WHERE user_id = '" . $_SESSION['user_id']. "';");
        header('Location: home.php ');
        die();
   }else {
