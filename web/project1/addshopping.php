a91d6f116eddc30606680d06e3f64c2e931f26f6
Sheena Jessee <calalilym@yahoo.com>
2019-02-23 14:37:26 -0700

    will it work now

--- web/project1/addshopping.php
+++ web/project1/addshopping.php
@@ -4,8 +4,9 @@
    require "config.php";
    $db=getDb();
    $_SESSION['shoppingList']= array();
-   $item = htmlspeccialchars($_GET['addto']);
+   $item = htmlspecialchars($_GET['addto']);
    echo $item;
+   $_SESSION['shoppingList']= array();
    array_push($_SESSION['shoppingList'], $item);
    print_r($_SESSION['shoppingList']);
    //header('Location: shoppingList.php');
