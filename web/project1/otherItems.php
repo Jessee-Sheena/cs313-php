01f98806cd44650fd056121fcc7fc5e3f3b628ba
Sheena Jessee <calalilym@yahoo.com>
2019-02-23 15:41:10 -0700

    sdjfl

--- web/project1/otherItems.php
+++ web/project1/otherItems.php
@@ -6,7 +6,7 @@
    <div id="pictureRow">
    <h1> Other Recipe's  of Interest</h1>
    <?php
-      echo "what"
+      echo "what";
    foreach($db->query("SELECT recipe_id, image FROM recipe ORDER BY recipe_id DESC LIMIT 5;")as $row) {
    ?>
      <a href="setsessionid.php?id="<?php $row['recipe_id'] ?>" <img src="<?php echo $row['image'] ?>" alt= "food"/>
