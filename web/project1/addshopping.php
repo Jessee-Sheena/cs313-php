4eaa54579e9d9feb90bcffd022e9bedbcf6667d3
Sheena Jessee <calalilym@yahoo.com>
2019-02-23 14:14:56 -0700

    laskdj

--- web/project1/addshopping.php
+++ web/project1/addshopping.php
@@ -4,7 +4,9 @@
    require "config.php";
    $db=getDb();
    $_SESSION['shoppingList']= array();
-   array_push($_SESSION['shoppingList'], htmlspeccialchars($_POST['addto']));
+   $item = htmlspeccialchars($_POST['addto']);
+   echo $item;
+   array_push($_SESSION['shoppingList'], $item);
    print_r($_SESSION['shoppingList']);
    //header('Location: shoppingList.php');
    //die();
