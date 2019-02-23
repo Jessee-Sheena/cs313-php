82c6f62c933edd9cdff0ce99edc342fd045b0566
Sheena Jessee <calalilym@yahoo.com>
2019-02-23 15:51:18 -0700

    i am so over this

--- web/project1/otherItems.php
+++ web/project1/otherItems.php
@@ -1,12 +1,13 @@
+  <link rel="stylesheet" href="home.css" />
 <?php 
    session_start();      
    require "config.php";
    $db=getDb();
    ?>
    <div id="pictureRow">
-   <h1> Other Recipe's  of Interest</h1>
+   <h2> Other Recipe's  of Interest</h2>
    <?php
-      echo "what";
+      
    foreach($db->query("SELECT recipe_id, image FROM recipe ORDER BY recipe_id DESC LIMIT 5;")as $row) {
    ?>
      <a href="setsessionid.php?id="<?php echo $row['recipe_id'] ?>"> <img src="<?php echo $row['image'] ?>" alt= "food"/></a>
