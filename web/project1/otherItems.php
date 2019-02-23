57402b5a751f02da002bc0976c33835486007900
Sheena Jessee <calalilym@yahoo.com>
2019-02-23 16:04:43 -0700

    ;sdjfsdkj

--- web/project1/otherItems.php
+++ web/project1/otherItems.php
@@ -4,15 +4,13 @@
    require "config.php";
    $db=getDb();
    ?>
-   <div id="pictureRow">
    <h2> Other Recipe's  of Interest</h2>
-   <?php
-      
-   foreach($db->query("SELECT recipe_id, image FROM recipe ORDER BY recipe_id DESC LIMIT 5;")as $row) {
+   <div id="pictureRow">      
+   <?php      
+      foreach($db->query("SELECT recipe_id, image FROM recipe ORDER BY recipe_id DESC LIMIT 5;")as $row) {
    ?>
-     <div class="pict">
-     <a href="setsessionid.php?id="<?php echo $row['recipe_id'] ?>"> <img src="<?php echo $row['image'] ?>" alt= "food"/></a>
-     </div>
-  <?php }  
-   ?>
+        <div class="pict">
+          <a href="setsessionid.php?id="<?php echo $row['recipe_id'] ?>"> <img src="<?php echo $row['image'] ?>" alt= "food"/></a>
+       </div>
+  <?php } ?>
    </div>
