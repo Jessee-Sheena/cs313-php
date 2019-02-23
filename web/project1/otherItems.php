e9774fa3b5360c6206abd3c6452d9fe2196469b5
Sheena Jessee <calalilym@yahoo.com>
2019-02-23 15:34:30 -0700

    changes

--- web/project1/otherItems.php
+++ web/project1/otherItems.php
@@ -7,7 +7,7 @@
    <h1> Other Recipe's  of Interest</h1>
    <?php
    $num;
-   $items = $db->prepare("SELECT recipe_id, recipe_image FROM recipe ORDER BY recipe_id DESC LIMIT 5");
+   $items = $db->query("SELECT recipe_id, recipe_image FROM recipe ORDER BY recipe_id DESC LIMIT 5");
    $items->execute();
    foreach($items as $row) {
    ?>
