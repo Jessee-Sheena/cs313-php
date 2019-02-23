7c35f036824bc133a800feb5c0be954738a6f05e
Sheena Jessee <calalilym@yahoo.com>
2019-02-23 11:13:46 -0700

    what

--- web/project1/recipeInfo.php
+++ web/project1/recipeInfo.php
@@ -60,7 +60,7 @@
                  WHERE r.recipe_id =". $_SESSION['id'] ."AND s.section_id = 1
                  ORDER BY q.ingredient_id ASC;");
           $array->execute();      
-          $mainIngredients = $array->fetch_all();
+          $mainIngredients = $array->fetchAll();
           print_r($mainIngredients);
          ?>
         
