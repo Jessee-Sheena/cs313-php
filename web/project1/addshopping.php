492b8971abd4f0008d0c1b7803cc9e9fcdda12d3
Sheena Jessee <calalilym@yahoo.com>
2019-02-23 14:09:10 -0700

    why

--- web/project1/addshopping.php
+++ web/project1/addshopping.php
@@ -5,4 +5,7 @@
    $db=getDb();
    $_SESSION['shoppingList']= array();
    array_push($_SESSION['shoppingList'], htmlspeccialchars($_POST['addto']));
+   print_r($_SESSION['shoppingList']);
+   //header('Location: shoppingList.php');
+   //die();
    ?>
