45a0e61fdf8151833f31dbb81e5e78b104e49f69
Sheena Jessee <calalilym@yahoo.com>
2019-02-22 21:25:49 -0700

    try this

--- web/project1/recipelist.php
+++ web/project1/recipelist.php
@@ -7,7 +7,7 @@
    foreach ($db->query("SELECT recipe_name, recipe_id, image, recipe_description FROM recipe") as $row)
    {
        echo '<div class="listSection"><img src="'.$row['image'] . '" alt= "food"/>';                      
-       echo '<h2 class="listHeader"><a href="recipeInfo.php?id='.$row['recipe_id'].'">'. $row['recipe_name'] . '</a></h2>';
+       echo '<h2 class="listHeader"><a href="setsessionid.php?id='.$row['recipe_id'].'">'. $row['recipe_name'] . '</a></h2>';
        echo '<p>'.$row['recipe_description']. '</p></div>';        
    }		
 
